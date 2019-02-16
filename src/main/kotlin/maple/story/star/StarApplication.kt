15e1bfce48afda882b03f31d1186a89b1fabfa4d
lexcao <lexcao@foxmail.com>
2019-02-16 14:18:44 +0800

    first commit.

---
+++ src/main/kotlin/maple/story/star/StarApplication.kt
@@ -0,0 +1,12 @@
+package maple.story.star
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarApplication
+
+fun main(args: Array<String>) {
+    runApplication<StarApplication>(*args)
+}
+
